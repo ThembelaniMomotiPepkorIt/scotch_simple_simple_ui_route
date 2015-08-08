9d9f6f62e9571b9cc7e6502bbb47d652b617210f
Thembelani <lonwabo.momoti@gmail.com>
2015-08-08 20:35:03 +0200

    08/08/2015 Changes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var routeApp = angular.module('routeApp',['ui-router']);
 
+routeApp.controller('HomeListController', function($scope){
+	$scope.dogs = ['German Sheperd','Poodle','Jack Russel'];
+});
+
+//routeApp.controller('')
 
 routeApp.config(function($stateProvider, $urlRouterProvider){
 
@@ -9,6 +14,15 @@ routeApp.config(function($stateProvider, $urlRouterProvider){
 		.state('home',{
 			url: '/home',
 			templateUrl: 'partial-home.html'
+		})
+		.state('home.list',{
+			url :'/list',
+			template : 'partial-home-list.html',
+			controller : 'HomeListController'
+		})
+		.state('home-paragraph', {
+			url: '/paragraph',
+			template :'I could use a drink right now'
 		})
 		
 		.state('about',{
